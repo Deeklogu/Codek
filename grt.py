2ca18e5fddb703687035030e2741b7a79191ec54
Deeklogu <dksha2583151@gmail.com>
2017-06-21 12:07:21 +0530

    Create grt.py

---
+++ grt.py
@@ -0,0 +1,9 @@
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+c=int(input("enter thrid number"))
+if a>b and a>c:
+    print(str(a) +" is greater")
+elif b>a and b>c:
+    print(str(b) +" is greater")
+else:
+    print(str(c) +" is greater")
